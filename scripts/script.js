ea7f777c2bd23249d757ca35350bbf5a28ecc113
Daniel Beauchamp <daniel.beauchamp@gmail.com>
2020-01-21 00:00:41 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+const CameraInfo = require('CameraInfo');
+
+
+// Load in the patches module
+const Patches = require('Patches');
+
+Patches.setBooleanValue('isRecordingVideo', CameraInfo.isRecordingVideo);
